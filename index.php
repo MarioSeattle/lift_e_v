4127b23a05ea001316a9b1052572e134023d8858
Rosie <efishe03@seattlecentral.edu>
2016-10-13 20:37:42 -0700

    index.php
    index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>   
 
-<!------#content------>
-        <div id="content">
+<!------#------>
+        <div id="">
             
         <?php if (have_posts()) : while (have_posts()) : the_post();?>
             
@@ -16,8 +16,8 @@
             
         <?php endwhile;?>
         <?php endif; ?>
-            </div><!--end #content-->
-<!-------end #content------>
+            </div><!--end #-->
+<!-------end #------>
 
 <?php get_sidebar(); ?>
 
