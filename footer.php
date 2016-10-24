b63db184d7e92d4d66cbe81605cb571b04fbd036
MarioSeattle <marioseattle@live.com>
2016-10-23 20:45:09 -0700

    Added Header Nav + Footer   + CSS

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 </div><!--end #middle-->
-    
+
 <!---begin WP Footer Function--->
-    <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 <!---end WP Footer Function--->
-    
+<footer>
+    <p><a href="#">Contact Us</a> | <a href="#">Sitemap</a> | <a href="#">Privacy Policy</a></p>
+    <p>&copy;2014 Copyright info here...</p>
+</footer>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Begin Meta -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <title>Advance Web Dev Fall 2016</title>
     <meta name="description" content="<?php echo get_the_excerpt(); ?>" />
-<!-- End Meta -->
-<title><?php get_my_title_tag(); ?></title>
-    
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
     <link href="css/style.css" rel="stylesheet" type="text/css" />
-    
-    
+
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
 
-    
-<!---begin WP Head Function--->
+    <!---begin WP Head Function--->
     <?php wp_head(); ?>
-<!---end WP Head Function--->
-     
+    <!---end WP Head Function--->
+
 </head>
+
 <body <?php body_class();?>>
-        
-        
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'div', 'container_id' => 'nav', )); ?>
-       
-<div id="middle">
+
+<h1>Advance Web Dev </h1>
+
+<header>
+    <nav>
+        <ul class="nav inline-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Photos</a></li>
+            <li><a href="#">Trips</a></li>
+        </ul>
+    </nav>
+</header>
