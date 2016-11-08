2fe01256cbb029c90d01602aec1a181574121ab2
MarioSeattle <marioseattle@live.com>
2016-11-07 19:55:43 -0800

    sidebar tag

--- page.php
+++ page.php
@@ -94,10 +94,7 @@
     </main>
 
     <sidebar id="primary">
-        <h4>Recent Blog Post</h4>
-        <img src="images/twitter-logo.jpg" alt="test img">
-        <p><strong>Blog Post Title</strong></p>
-        <p>Author, Sept 20 2016</p>
+        <?php get_sidebar(); ?>
 
     </sidebar>
 
