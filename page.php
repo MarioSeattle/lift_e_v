a70693792c0fb2b5ec5910274a574f4c1338e882
MarioSeattle <marioseattle@live.com>
2016-11-10 12:46:24 -0800

    php get_sidebar

--- page.php
+++ page.php
@@ -94,11 +94,7 @@
 </main>
 
 <sidebar id="primary">
-    <h4>Recent Blog Post</h4>
-    <img src="images/twitter-logo.jpg" alt="test img">
-    <p><strong>Blog Post Title</strong></p>
-    <p>Author, Sept 20 2016</p>
-
+    <?php get_sidebar(); ?>
 </sidebar>
 
 <?php get_footer(); ?>
