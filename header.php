8770fdf3a28c14fc82ddb60635d4d4dcc8c334da
brownae <aebrown9@gmail.com>
2016-12-02 17:39:57 -0800

    Beautiful Nav bar!

--- header.php
+++ header.php
@@ -17,16 +17,27 @@
     <script src="<?php bloginfo('template_directory'); ?>/js/html5.js" type="text/javascript"></script>
     <!--[if lt IE 9]>
     <![endif]-->
-    
+
     <?php wp_head(); ?>
 </head>
 <body>
 <!--Header starts here-->
 <header>
+
+  <nav class="mobile">
+       <label for="drop" class="toggle"><img src="<?php echo get_template_directory_uri(); ?>/images/hamburger-menu.png"></label>
+       <input type="checkbox" id="drop" />
+      <?php wp_nav_menu( array(
+    'theme_location' => 'main-menu' ,
+    'menu' => 'Main Menu' ,
+    'container'  => 'ul',
+    ) ); ?>
+   </nav>
+
     <h1>
         <a href="<?php bloginfo('url'); ?>">
             <span>Lift every Voice foundation</span> <!-- for SEO -->
-            
+
             <img src="<?php $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id(get_option('page_on_front')), "Full");
             echo $imgsrc[0]; ?>" id="logo-main"
                  alt="Lift Every Voice Foundation Logo" >
@@ -40,12 +51,12 @@
    <nav class="desktop">
         <label for="drop" class="toggle"><img src="<?php echo get_template_directory_uri(); ?>/images/hamburger-menu.png"></label>
         <input type="checkbox" id="drop" />
-       <?php wp_nav_menu( array( 
+       <?php wp_nav_menu( array(
 			'theme_location' => 'main-menu' ,
 			'menu' => 'Main Menu' ,
 			'container'  => 'ul',
 		) ); ?>
     </nav>
-    
+
 <!--Header Ends here-->
 </header>
